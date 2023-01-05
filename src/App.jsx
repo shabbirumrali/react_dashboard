40d4868bf04ddfcfba0ca31d380e27a914bd25c9
Shabbir Umrali <sumraliwala.contractor@libertyglobal.com>
2023-01-05 18:42:00 +0530

    Folder Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import './App.scss'
+import { Admin, BarGraph, Chat, Compose, Email, Plugins, Stock, System } from './pages'
 
 const App = () => {
   return (
     <div>
       <h1>App</h1>
+
+      <Admin />
+      <BarGraph />
+      <Chat />
+      <Compose />
+      <Email />
+      <Plugins />
+      <Stock />
+      <System />
+      
     </div>
   )
 }
---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+export { default as Admin } from './Admin/Admin.jsx'
+export { default as BarGraph } from './BarGraph/BarGraph'
+export { default as Chat } from './Chat/Chat'
+export { default as Compose } from './Compose/Compose'
+export { default as Email } from './Email/Email'
+export { default as Plugins } from './Plugins/Plugins'
+export { default as Stock } from './Stock/Stock'
+export { default as System } from './System/System'
