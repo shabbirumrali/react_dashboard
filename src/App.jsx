cfa5a17908532b29c54a9591709a810bafcd5c2c
Shabbir Umrali <sumraliwala.contractor@libertyglobal.com>
2023-01-23 14:47:18 +0530

    Folder updates and created left sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.scss'
-import { Admin, BarGraph, Chat, Compose, Email, Plugins, Stock, System } from './pages'
+// import { Admin, BarGraph, Chat, Compose, Email, Plugins, Stock, System } from './pages'
+import { LeftSideBar, Navigation } from './components'
 
 const App = () => {
   return (
     <div>
       <h1>App</h1>
-
+      <LeftSideBar />
+{/*         
       <Admin />
       <BarGraph />
       <Chat />
@@ -13,7 +15,7 @@ const App = () => {
       <Email />
       <Plugins />
       <Stock />
-      <System />
+      <System /> */}
       
     </div>
   )
--- src/components/LeftSideBar/LeftSideBar.jsx
+++ src/components/LeftSideBar/LeftSideBar.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import {RiWechatLine} from 'react-icons/ri'
+import {BsChatLeftText} from 'react-icons/bs'
+import {FiUsers,FiHelpCircle,FiSettings} from 'react-icons/fi'
+import{AiOutlineSend,AiOutlineStock} from 'react-icons/ai'
+import{MdSystemUpdate} from 'react-icons/md'
+import{BsFileBarGraphFill} from 'react-icons/bs'
 import './LeftSideBar.scss'
 
 const LeftSideBar = () => {
   return (
-    <div>LeftSideBar</div>
+    <div className='dialogue-box'>
+      <div className='chat-logo'><RiWechatLine/></div>
+      <div className='chat-icon'><BsChatLeftText/></div>
+      <div className='admin-icon'><FiUsers/></div>
+      <div className='send-icon'><AiOutlineSend/></div>
+      <div className='system-icon'><MdSystemUpdate/></div>
+      <div className='stocks-icon'><AiOutlineStock/></div>
+      <div className='bargraph-icon'><BsFileBarGraphFill/></div>
+      <div className='help-icon'><FiHelpCircle/></div>
+      <div className='settings-icon'><FiSettings/></div>
+    </div>
   )
 }
 
